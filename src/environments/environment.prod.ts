d7a50ab1c59cf613b5540f294d1f382521e1c26b
Bradley-Walker <bradleynwalker1@gmail.com>
2018-07-28 16:37:14 -0400

    Fixed the apiUrl property

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://revachatbackendelasticbeanstalk-env.nvwhqpvpsz.us-east-1.elasticbeanstalk.com'
+  apiUrl: 'http://revachatbackendelasticbeanstalk-env.nvwhqpvpsz.us-east-1.elasticbeanstalk.com/'
 };
